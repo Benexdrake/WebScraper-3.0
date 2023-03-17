aef12f1b85bfcd77dbabd59f530d32f2b1bd3241
BeneX Drake <50136177+Benexdrake@users.noreply.github.com>
2023-03-17 14:51:13 +0100

    much changes and new Bots

---
+++ Webscraper.Tests/CrunchyrollTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Webscraper.Tests;
+
+public class CrunchyrollTest
+{
+    private readonly ICR_API _api;
+
+    public CrunchyrollTest(ICR_API api)
+    {
+        _api = api;
+    }
+
+    [Fact]
+    public async Task GetAnimeByUrl()
+    {
+        var anime = await _api.GetAnimeByUrlAsync("https://www.crunchyroll.com/de/series/GYEX24PV6/digimon-adventure-2020", 3000);
+        Assert.NotNull(anime);
+    }
+}
--- Webscraper.Tests/Usings.cs
+++ Webscraper.Tests/Usings.cs
@@ -1,1 +1,32 @@
 global using Xunit;
+global using Webscraper.API;
+
+global using Webscraper.API.Interfaces;
+global using Webscraper.API.Scraper.Amazon.Controllers;
+global using Webscraper.API.Scraper.Crunchyroll.Controllers;
+global using Webscraper.API.Scraper.Dota2.Controllers;
+global using Webscraper.API.Scraper.Honda.Controllers;
+global using Webscraper.API.Scraper.IMDB.Controllers;
+global using Webscraper.API.Scraper.OnePlus.Controllers;
+global using Webscraper.API.Scraper.Pokemons.Controller;
+global using Webscraper.API.Scraper.Steam.Controllers;
+global using Webscraper.API.Scraper.TCG_Magic.Controller;
+global using Webscraper.API.Scraper.TCG_Pokemon.Controller;
+global using Webscraper.API.Scraper.TVProgramm.Controllers;
+global using Webscraper.API.Scraper.Twitch.Controllers;
+
+global using Webscraper.Models.Amazon.Models;
+global using Webscraper.Models.Crunchyroll.Models;
+global using Webscraper.Models.Crunchyroll.BuildModels;
+global using Webscraper.Models.Dota2.Models;
+global using Webscraper.Models.Honda.Models;
+global using Webscraper.Models.Insight_Digital_Handy.Models;
+global using Webscraper.Models.IMDB.BuildModels;
+global using Webscraper.Models.IMDB.Models;
+global using Webscraper.Models.Pokemons.Builder;
+global using Webscraper.Models.Pokemons.Models;
+global using Webscraper.Models.Steam.Models;
+global using Webscraper.Models.TCG_Magic.Model;
+global using Webscraper.Models.TCG_Pokemon.Models;
+global using Webscraper.Models.TVProgramm.Models;
+global using Webscraper.Models.Twitch.Models;
